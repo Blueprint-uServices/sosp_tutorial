83a2e2b80f4dd33ca282061956174a1c57f3bedf
Vaastav Anand <vaastav.anand05@gmail.com>
2024-10-29 17:14:24 +0100

    Add part 2 solution

--- hotel/wiring/specs/tracing.go
+++ hotel/wiring/specs/tracing.go
@@ -1,8 +1,15 @@
 package specs
 
 import (
+	"fmt"
+
 	"github.com/blueprint-uservices/blueprint/blueprint/pkg/wiring"
 	"github.com/blueprint-uservices/blueprint/plugins/cmdbuilder"
+	"github.com/blueprint-uservices/blueprint/plugins/goproc"
+	"github.com/blueprint-uservices/blueprint/plugins/http"
+	"github.com/blueprint-uservices/blueprint/plugins/jaeger"
+	"github.com/blueprint-uservices/blueprint/plugins/linuxcontainer"
+	"github.com/blueprint-uservices/blueprint/plugins/opentelemetry"
 	"github.com/blueprint-uservices/blueprint/plugins/workload"
 	"github.com/blueprint-uservices/sosp_tutorial/hotel/workload/workloadgen"
 )
@@ -21,16 +28,21 @@ var Tracing = cmdbuilder.SpecOption{
 func applyTracingScaffolding(spec wiring.WiringSpec, serviceName string, collectorName string) string {
 	// Part 2: TODO ---> Complete the function to apply the necessary plugins
 
+	procName := fmt.Sprintf("%s_process", serviceName)
+	ctrName := fmt.Sprintf("%s_container", serviceName)
+
 	// Step 1: Apply the opentelemetry plugin
+	opentelemetry.Instrument(spec, serviceName, collectorName)
 
 	// Step 2: Apply the http plugin
+	http.Deploy(spec, serviceName)
 
 	// Step 3: Apply the goproc plugin
+	goproc.CreateProcess(spec, procName, serviceName)
 
 	// Step 4: Apply the linuxcontainer plugin
-
 	// Step 5: Replace the serviceName with the containerName in the return value
-	return serviceName
+	return linuxcontainer.CreateContainer(spec, ctrName, procName)
 }
 
 func makeTracingSpec(spec wiring.WiringSpec) ([]string, error) {
@@ -38,7 +50,7 @@ func makeTracingSpec(spec wiring.WiringSpec) ([]string, error) {
 	services := initServices(spec)
 
 	// Part 2: TODO ---> Initialize a jaeger collector that can be used for collecting traces
-	collector := "" // TODO: Replace this with a call to the jaeger plugin
+	collector := jaeger.Collector(spec, "jaeger") // TODO: Replace this with a call to the jaeger plugin
 
 	cntrs := []string{}
 	for _, service := range services {
